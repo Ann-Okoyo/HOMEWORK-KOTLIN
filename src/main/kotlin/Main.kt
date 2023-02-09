50db71d7c12ac24b7e32fceb145d943d1a1a476f
Muyale Okoyo <annmuyale@gmail.com>
2023-02-09 16:55:05 +0300

    Functions

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,22 @@
+fun main() {
+
+   println( div(21.0,3.0))
+    println( div(30.0,3.0))
+
+
+
+   println(names("Ann"))
+}
+
+fun names( names:String) {
+    var fullname = ("hello" + " " + names)
+    println(fullname)
+}
+
+fun div(num1:Double ,num2:Double )  :Double{
+    return num1%num2
+
+
+
+}
+fun main()
